ba09640d669f86922020121b68f7a62e1f11461a
Rosonlee <rosonlee98@gmail.com>
2020-06-01 17:08:10 +0800

    ‘updataed

---
+++ Chapter02/5.30.py
@@ -0,0 +1,13 @@
+import KNN
+# import importlib,sys
+
+# group, labels = KNN.createDataSet()
+
+# KNN.classify0([0, 0],dd group, labels, 3)
+
+# datingDataMat, datingLabels = KNN.file2matrix('datingTestSet.txt')
+
+# KNN.datingClassTest()
+# KNN.classifyPerson()
+
+testVector = KNN.img2Vector('testDigits/0_13.txt')
